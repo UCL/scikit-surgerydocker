cbd1c9a03aadd62a295487f58b2bb3a745ad95e9
Mian Ahmad <mianasbat@gmail.com>
2020-11-20 00:18:26 +0000

    output text updated.

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ def main():
    with open('output/file.txt', 'w+') as outputfile:
       outputfile.write(contents)
       for item in range(0,5):
-         outputfile.write('\nThis line is appended by the program')
+         outputfile.write('\nText is copied from input/file.txt and \
+         pasted to a new file output/file.txt along with these 5 new lines.')
 
 if __name__ == '__main__':
    main()
