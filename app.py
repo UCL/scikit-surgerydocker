d81089f140ad08db0c417f8795d33cd1c15374e3
Mian Ahmad <mianasbat@gmail.com>
2020-12-04 12:06:53 +0000

    path to input and output folder changed

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 def main():
 
-   with open('input/inputfile.txt', 'r') as inputfile:
+   with open('./input/inputfile.txt', 'r') as inputfile:
       contents = inputfile.read()
 
-   with open('output/outputfile.txt', 'w+') as outputfile:
+   with open('./output/outputfile.txt', 'w+') as outputfile:
       outputfile.write(contents)
       for item in range(0,5):
          outputfile.write('\nText is copied from input/file.txt and \
