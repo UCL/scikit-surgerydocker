2b86b105aa3c3d9b094653b6be4b9c5dc9b31dc5
Mian Ahmad <mianasbat@gmail.com>
2021-01-04 10:38:53 +0000

    Issue #3: app split into read and write function

--- project/app.py
+++ project/app.py
@@ -1,41 +1,29 @@
 """This module gives a simple example to demonstrate how to structure
 the project to effectively work with any third party data. That includes
-reading the data and writing the results.  An input folder with a small
+reading data from input file and writing results to output file. An input folder with a small
 text file and an output folder is included in the repo"""
 
 
-def main():
-   """The main function that reads the data from input file in the
-   input folder, process it and store the results in the output file
-   in the output folder. The input folder, inputfile.txt and output
-   folder already exist.
+def read_file(file_path, file_name):
    
-   Args:
-    None
-   Returns:
-    None
-   Raises:
-    IOError: An error occurred accessing the input/inputfile.txt.
-    
-   
-   """
-
-
-   
-   contents = ''
-
+   full_path = file_path + file_name
    try:
-      with open('./input/inputfile.txt', 'r') as inputfile:
-         contents = inputfile.read()
+      with open(full_path, 'r') as input_file:
+         return input_file.read()
    except IOError:
       print('An error occurred accessing the input/inputfile.txt')
+   
 
+
+
+def write_file(file_path, file_name):
+   full_path = file_path + file_name
    try:
-      with open('./output/outputfile.txt', 'w+') as outputfile:
-         outputfile.write(contents)
+      with open(full_path, 'w+') as output_file:
+         output_file.write(contents)
          for item in range(0,5):
-            outputfile.write('\nText is copied from input/file.txt and \
-               pasted to a new file output/file.txt along with these 5 new lines.')
+            output_file.write('\nAppended line by write_file function.')
+
       print('A new file is created successfully in ./output directory')
 
    except:
@@ -43,4 +31,6 @@ def main():
 
 
 if __name__ == '__main__':
-   main()
+   contents = None
+   contents = read_file(file_path='./input/', file_name='input_file.txt')
+   write_file(file_path='./output/', file_name='output_file.txt')
