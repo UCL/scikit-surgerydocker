d076b4a526360622f94b06a39586aab179a105e4
Mian Ahmad <mianasbat@gmail.com>
2021-03-02 12:04:59 +0000

    Issue #31: initial commit

---
+++ .docs/docker-config.py
@@ -0,0 +1,6 @@
+def main():
+    print('Welcome to the Docker configuration script.')
+    print('Answer each questions from the given options or choose NULL \nto get the Dockerfile according to your requirements.')
+
+if __name__=='__main__':
+    main()
